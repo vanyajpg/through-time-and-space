499dd1b3116ff4fe18366344557018bf8f49df39
Vanya <113653431+vanyajpg@users.noreply.github.com>
2023-09-20 00:47:53 -0400

    blast offff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null)
+  const [dateInput, setDateInput] = useState('')
+
+  function handleButton(){
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=OBTkyKhEQeHyGV3oObYAaObQSeu5Ig4BpuaNudlP&date=${dateInput}`)
+    .then(res => res.json())
+    .then(data => {
+      setData(data)
+    })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }
+
+  function handleDateInputChange(event){
+    setDateInput(event.target.value)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>explore the universe</h1>
+      <h3>enter your date</h3>
+
+      <input 
+      type ="date" 
+      value={dateInput} 
+      onChange={handleDateInputChange}
+      />
+
+      <button onClick={handleButton}>explore</button>
+
+      {data && (
+      <div>
+        <h2>{data.title}</h2>
+        <img src={data.hdurl} alt="APOD" width="500px" height="500px" />
+        <h2>Description from {(dateInput)}</h2>
+        <h3>{data.explanation}</h3>
+      </div>
+      )}
+
     </div>
-  );
+  )
 }
 
 export default App;
+
+// ===========================
+// ===========================
+
+
+
+// ===========================
+// ===========================
+
