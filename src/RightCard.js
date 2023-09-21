dd5b313a260b735bbb4cd8305bfe63bb38d1fbfc
Vanya <113653431+vanyajpg@users.noreply.github.com>
2023-09-20 22:05:49 -0400

    happy with this

--- src/RightCard.js
+++ src/RightCard.js
@@ -7,35 +7,40 @@ export default function RightCard({data, showImage, showDate, dateInput}) {
 
   return (
     <div className='mx-6 mt-12 lg:mt-0'>
-        <h2 className='text-center text-3xl text-[#cddaea] font-semibold m-4'>{data.title}</h2> 
         <div className=' text-[#cddaea] p-2 '>
           {data && (
-            <ReactCardFlip isFlipped={flip}
-                flipDirection="vertical">
-                <div className='text-center'>
-                    <div> {showImage && (
-                        <>
-                        <img src={data.hdurl} alt="APOD"
-                            className=' mx-auto rounded w-full h-[500px] border-2 border-[#3a3a40] shadow-md hover:shadow-2xl transition duration-300 ' />
-                        </>
-                        )}
+            <div>
+                <div>
+                    <h2 className='text-center text-3xl text-[#cddaea] font-semibold m-4'>{data.title}</h2> 
+                </div>
+                
+                <ReactCardFlip isFlipped={flip}
+                    flipDirection="vertical">
+                    <div className='text-center'>
+                        <div> {showImage && (
+                            <>
+                            <img src={data.hdurl} alt="APOD"
+                                className=' mx-auto rounded w-full h-[500px] border-2 border-[#3a3a40] shadow-md hover:shadow-2xl transition duration-300 ' />
+                            </>
+                            )}
 
-                    </div> 
-                    <br />
-                    <br />
-                    <button className='bg-[#737484] hover:bg-[#d9d9de] bg-opacity-30 hover:bg-opacity-10 px-12 py-2 rounded transition duration-300 mb-8' onClick={() => setFlip(!flip)}>
-                        Flip</button>
-                </div>
-                <div className=''>
-                    <div className=' w-full text-center'>
-                        <p className="text-lg">{data.explanation}</p>
+                        </div> 
                         <br />
-                        <button className=' text-lg bg-[#737484] hover:bg-[#d9d9de] bg-opacity-30 hover:bg-opacity-10 px-12 py-2 rounded transition duration-300 mb-8 ' onClick={() => setFlip(!flip)}>
-                            Flip
-                        </button>
+                        <br />
+                        <button className='bg-[#737484] hover:bg-[#d9d9de] bg-opacity-30 hover:bg-opacity-10 px-12 py-2 rounded transition duration-300 mb-8' onClick={() => setFlip(!flip)}>
+                            Flip</button>
                     </div>
-                </div>
-            </ReactCardFlip>
+                    <div className=''>
+                        <div className=' w-full text-center'>
+                            <p className="text-lg">{data.explanation}</p>
+                            <br />
+                            <button className=' text-lg bg-[#737484] hover:bg-[#d9d9de] bg-opacity-30 hover:bg-opacity-10 px-12 py-2 rounded transition duration-300 mb-8 ' onClick={() => setFlip(!flip)}>
+                                Flip
+                            </button>
+                        </div>
+                    </div>
+                </ReactCardFlip>
+            </div>
         )}
         </div>
     </div>
