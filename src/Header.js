6dd70a4224d5c49bf7cd5d3b9631f4d45a560703
Vanya <113653431+vanyajpg@users.noreply.github.com>
2023-09-20 22:17:51 -0400

    fine tuning

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className=''>
-      <div className='text-[#cddaea] mx-auto pt-6'>
+      <div className='text-[#cddaea] mx-auto pt-12'>
           <h1 className='bg-gradient-to-r from-[#f4f6fa] via-[#a2bad7] to-[#3b608e] text-transparent bg-clip-text capitalize flex justify-center text-6xl font-semibold underline hover:bg-gradient-to-r hover:from-[#f9f9f9] hover:via-[#c8d8ea] hover:to-[#527095] text-center '>Through Space & Time</h1>
       </div>
     </div>
