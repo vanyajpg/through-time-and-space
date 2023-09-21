fc313b66975d5b610ea7f345ffab9ec81747c676
Vanya <113653431+vanyajpg@users.noreply.github.com>
2023-09-21 00:02:07 -0400

    recovering lost info

--- src/RightCard.js
+++ src/RightCard.js
@@ -11,7 +11,7 @@ export default function RightCard({data, showImage, showDate, dateInput}) {
           {data && (
             <div>
                 <div>
-                    <h2 className='text-center text-3xl text-[#cddaea] font-semibold m-4'>{data.title}</h2> 
+                    <h2 className='text-center text-3xl text-[#cddaea] font-semibold m-4 transform hover:scale-110 transition-transform duration-500 '>{data.title}</h2> 
                 </div>
                 
                 <ReactCardFlip isFlipped={flip}
@@ -20,21 +20,21 @@ export default function RightCard({data, showImage, showDate, dateInput}) {
                         <div> {showImage && (
                             <>
                             <img src={data.hdurl} alt="APOD"
-                                className=' mx-auto rounded w-full h-[500px] border-2 border-[#3a3a40] border-opacity-50 shadow-md hover:shadow-2xl transition duration-300 ' />
+                                className=' mx-auto rounded w-full h-[500px] border-2 border-[#3a3a40] border-opacity-50 shadow-md hover:shadow-2xl transition duration-300 -mb-6' />
                             </>
                             )}
 
                         </div> 
                         <br />
                         <br />
-                        <button className='bg-[#737484] hover:bg-[#d9d9de] bg-opacity-30 hover:bg-opacity-10 px-12 py-2 rounded transition duration-300 mb-8' onClick={() => setFlip(!flip)}>
+                        <button className='bg-[#737484] hover:bg-[#d9d9de] bg-opacity-30 hover:bg-opacity-10 px-12 py-2  transition duration-300 mb-8 uppercase rounded-full font-semibold tracking-wider' onClick={() => setFlip(!flip)}>
                             Flip</button>
                     </div>
                     <div className=''>
                         <div className=' w-full text-center'>
                             <p className="text-lg">{data.explanation}</p>
                             <br />
-                            <button className=' text-lg bg-[#737484] hover:bg-[#d9d9de] bg-opacity-30 hover:bg-opacity-10 px-12 py-2 rounded transition duration-300 mb-8 ' onClick={() => setFlip(!flip)}>
+                            <button className=' text-lg bg-[#737484] hover:bg-[#d9d9de] bg-opacity-30 hover:bg-opacity-10 px-12 py-2 transition duration-300 mb-8 uppercase rounded-full font-semibold tracking-wider' onClick={() => setFlip(!flip)}>
                                 Flip
                             </button>
                         </div>
