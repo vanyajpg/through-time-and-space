588464272f0857ffa5243a5bc47d3b6eede2cc18
Vanya <113653431+vanyajpg@users.noreply.github.com>
2023-09-20 23:25:55 -0400

    more animations

--- src/RightCard.js
+++ src/RightCard.js
@@ -20,21 +20,21 @@ export default function RightCard({data, showImage, showDate, dateInput}) {
                         <div> {showImage && (
                             <>
                             <img src={data.hdurl} alt="APOD"
-                                className=' mx-auto rounded w-full h-[500px] border-2 border-[#3a3a40] shadow-md hover:shadow-2xl transition duration-300 ' />
+                                className=' mx-auto rounded w-full h-[500px] border-2 border-[#3a3a40] border-opacity-50 shadow-md hover:shadow-2xl transition duration-300 ' />
                             </>
                             )}
 
                         </div> 
                         <br />
                         <br />
-                        <button className='bg-[#737484] hover:bg-[#d9d9de] bg-opacity-30 hover:bg-opacity-10 px-12 py-2 rounded transition duration-300 mb-8' onClick={() => setFlip(!flip)}>
+                        <button className='bg-[#737484] hover:bg-[#d9d9de] bg-opacity-30 hover:bg-opacity-10 px-12 py-2 rounded-full transition duration-300 mb-8 font-semibold tracking-wider uppercase' onClick={() => setFlip(!flip)}>
                             Flip</button>
                     </div>
                     <div className=''>
                         <div className=' w-full text-center'>
-                            <p className="text-lg">{data.explanation}</p>
+                            <p className="text-lg pt-2">{data.explanation}</p>
                             <br />
-                            <button className=' text-lg bg-[#737484] hover:bg-[#d9d9de] bg-opacity-30 hover:bg-opacity-10 px-12 py-2 rounded transition duration-300 mb-8 ' onClick={() => setFlip(!flip)}>
+                            <button className=' text-lg bg-[#737484] hover:bg-[#d9d9de] bg-opacity-30 hover:bg-opacity-10 px-12 py-2 rounded-full transition duration-300 mb-8 font-semibold tracking-wider uppercase' onClick={() => setFlip(!flip)}>
                                 Flip
                             </button>
                         </div>
